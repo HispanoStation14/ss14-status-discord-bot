9fb3cd363f6574c1b1500f8a2f90301fe38b1d42
todotom <tomasdarioam@protonmail.com>
2023-11-26 16:57:37 +0100

    feat(security): read token from secret file

--- src/main.py
+++ src/main.py
@@ -22,4 +22,9 @@ async def status(ctx):
     await ctx.send(embed=generate_server_status_embed(server_status=server_status))
 
 
-bot.run(token=os.getenv("BOT_TOKEN"))
+discord_bot_token: str
+
+with open(os.getenv("DISCORD_BOT_TOKEN_FILE"), "r") as file:
+    discord_bot_token = file.read()
+
+bot.run(token=discord_bot_token)
